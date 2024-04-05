9b511545b023d88cf851f6ccb526d2e0b90266b7
Tomasz Kuzel <tomek.kuzel@gmail.com>
2024-04-05 16:30:43 +0200

    Add project files.

---
+++ Warehouse/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Warehouse
+{
+    class Program
+    {
+        const string FILE_NAME = "c:\\WarehouseFiles\\ImportFile.xlsx";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome in warehouse app.");
+            Console.WriteLine("Please make your selection.");
+            Console.WriteLine("1. Add item");
+            Console.WriteLine("2. Remove item");
+            Console.WriteLine("3. Check item");
+            Console.WriteLine("Press 1, 2 or 3.");
+
+            string choice = Console.ReadLine();
+
+            Console.WriteLine($"Your choice is: {choice}");
+
+            int chosenOption = 0;
+
+            Int32.TryParse(choice, out chosenOption);
+
+            Console.WriteLine("Please chose another option.");
+
+            choice = Console.ReadLine();
+
+            Int32.TryParse(choice, out chosenOption);
+
+        }
+    }
+
+}
