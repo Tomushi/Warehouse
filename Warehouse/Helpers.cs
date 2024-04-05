bd0b18c5dd7a8536c49b4d3cfed4867580a75194
Tomasz Kuzel <tomek.kuzel@gmail.com>
2024-04-05 16:45:44 +0200

    Lekcji ciag dalszy ...

---
+++ Warehouse/Helpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse
+{
+    public class Helpers
+    {
+        public int GiveMeNumber()
+        {        
+
+        int number = 5;
+        
+        return number;
+        }
+    }
+
+    public enum ItemTypes
+    {
+        Grocery,
+        Clothing,
+        Electronics,
+    }
+}
